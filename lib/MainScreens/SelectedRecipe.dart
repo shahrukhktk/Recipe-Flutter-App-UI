0cf43d4898e70bde098a082eaba1353caebed0db
Shahrukh <Shahrukhktk760@gmail.com>
2021-01-19 14:03:26 +0500

    INIT COMMIT 1. Auth Main Screen Done 2. Login Page Done 3. Signup option page done 4. Sign up with email page done 5. Upload image page done 6. Home Page done 7. Search Page done 8. Selected Recipe Page Inprogress

---
+++ lib/MainScreens/SelectedRecipe.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:recipe/AppAssets/app_assets.dart';
+import 'package:recipe/Authentication/signup.dart';
+import 'package:recipe/Authentication/signupoptionscreen.dart';
+
+class SelectedRecipeScreen extends StatefulWidget {
+  @override
+  _SelectedRecipeScreenState createState() => _SelectedRecipeScreenState();
+}
+
+class _SelectedRecipeScreenState extends State<SelectedRecipeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?cs=srgb&dl=pexels-myicahel-tamburini-1152994.jpg&fm=jpg'),
+            ),
+          )
+        ],
+        backgroundColor: Colors.white,
+      ),
+      body: ListView(
+        children: [
+          
+          Padding(
+            padding: const EdgeInsets.only(top: 20, right: 20, left: 20, bottom: 10,),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Grilled Chicken', style: TextStyle(
+                    fontSize: 25.0, fontFamily: 'Roboto', fontWeight: FontWeight.bold, color: Colors.black
+                ),),
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text('By John Doe', style: TextStyle(
+                      fontSize: 18.0, fontFamily: 'Roboto', fontWeight: FontWeight.w400, color: AppColors.themecolor
+                  ),),
+                ),
+              ],
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Container(
+              child: Image(
+                image: NetworkImage('https://images.pexels.com/photos/106343/pexels-photo-106343.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(top: 20, right: 20, left: 20, bottom: 10,),
+            child: Text('Upcoming class', style: TextStyle(
+                fontSize: 18.0, fontFamily: 'Roboto', fontWeight: FontWeight.w400, color: Colors.black
+            ),),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(top: 20, right: 20, left: 20, bottom: 10,),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: Icon(Icons.calendar_today_outlined, color: AppColors.themecolor,)),
+                Expanded(
+                  flex: 4,
+                  child: Text('Wed September 12', style: TextStyle(
+                      fontSize: 18.0, fontFamily: 'Roboto', fontWeight: FontWeight.w400, color: Colors.black
+                  ),),
+                ),
+
+                Expanded(
+                  flex: 3,
+                  child: GestureDetector(
+                    onTap: ()
+                    {
+                      Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SignUpOptionPage()),); //It should navigate to forgot password screen
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Register', style: TextStyle(
+                            fontFamily: 'Roboto', color: AppColors.accentcolor, fontSize: 16, fontWeight: FontWeight.w500, decoration: TextDecoration.underline
+                        ),),
+
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(Icons.arrow_forward, color: AppColors.accentcolor,),
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          )
+
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recipe/AppAssets/app_assets.dart';
-import 'package:recipe/MainScreens/HomePage.dart';
-import 'package:recipe/MainScreens/SearchPage.dart';
-import 'package:recipe/MainScreens/bottomnav.dart';
-
-import 'Authentication/getstarted.dart';
+import 'MainScreens/SelectedRecipe.dart';
 
 void main() => runApp(new MaterialApp(
   theme: ThemeData(
@@ -13,5 +9,5 @@ void main() => runApp(new MaterialApp(
     fontFamily: "Roboto",
   ),
   debugShowCheckedModeBanner: false,
-  home: SearchPage(),
+  home: SelectedRecipeScreen(),
 ));

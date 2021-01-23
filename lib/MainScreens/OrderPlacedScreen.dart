b55965a016a89e6d8c89a91b2729f0849487536c
Shahrukh <Shahrukhktk760@gmail.com>
2021-01-23 15:08:55 +0500

    INIT COMMIT 1. Auth Main Screen Done 2. Login Page Done 3. Signup option page done 4. Sign up with email page done 5. Upload image page done 6. Home Page done 7. Search Page done 8. Selected Recipe Page Done 9. Select Store in Done 10. Selected Store Details Done 11. Order Placed Screen Done

--- lib/MainScreens/OrderPlacedScreen.dart
+++ lib/MainScreens/OrderPlacedScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:recipe/AppAssets/DropDownwidget.dart';
 import 'package:recipe/AppAssets/app_assets.dart';
 import 'package:recipe/AppAssets/custom_cardlist.dart';
+import 'package:recipe/MainScreens/HomePage.dart';
 
 class OrderPlacedScreen extends StatefulWidget {
   @override
@@ -28,6 +29,63 @@ class _OrderPlacedScreenState extends State<OrderPlacedScreen> {
       body: ListView(
         children: [
 
+          Padding(
+            padding: const EdgeInsets.only(top: 40, bottom: 20.0),
+            child: Center(
+              child: Text("Order Placed", style: TextStyle(
+                fontFamily: 'Roboto', fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black
+              ),),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(child: Icon(Icons.check_circle_outline, color: AppColors.themecolor, size: 80,)),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(top: 40, bottom: 20.0),
+            child: Center(
+              child: Text("Your order will be on it's way soon!", style: TextStyle(
+                  fontFamily: 'Roboto', fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black
+              ),),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Center(child: Image(image: AssetImage('images/image.png'),)),
+          ),
+
+          //Order Now Btn
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, right: 20, left: 5, bottom: 10),
+            child: Center(
+              child: FlatButton(
+                height: 48,
+                minWidth: 135,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(35.0),
+                    side: BorderSide(color: AppColors.accentcolor)),
+                color: AppColors.accentcolor,
+                textColor: Colors.white,
+                padding: EdgeInsets.all(8.0),
+                onPressed: () {
+                  Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomePage()),);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Thank You",
+                    style: TextStyle(
+                      fontSize: 16.0, fontFamily: 'Roboto', fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
 
         ],
       ),
